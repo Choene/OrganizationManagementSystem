676048ae114aa0bd59696363172b1d09b42fed4c
Choene <cchokoe@yahoo.com>
2021-10-05 01:36:57 +0200

    Add project files.

---
+++ OrganizationManagementSystem/Migrations/20211004193421_FirstOMSMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrganizationManagementSystem.Migrations
+{
+    public partial class FirstOMSMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Did = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Did);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Eid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<double>(type: "float", nullable: false),
+                    Did = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Eid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ OrganizationManagementSystem/Program.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    //Step - 1 - Install 2 Packages:
+    //(1) Microsoft.EntityFrameworkCore.SqlServer
+    //(2) And Microsoft.EntityFrameworkCore.Tools
+
+    //Step - 2 - Creating Entities
+
+    [Table("Department")]
+    public class Department
+    {
+        [Key]
+        public int Did { get; set; }
+        public string DName { get; set; }
+        public string Description { get; set; }
+    }
+
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int Eid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+        public int Did { get; set; }
+    }
+
+    //Step - 3 - Creating Context Object -> Adding DbSets
+    public class OrganizationDbContext : DbContext
+    {
+        //Override DbContext
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=ROOT\\SQLEXPRESS;Database=OMSdb;Trusted_Connection=True;");
+        }
+
+        //DbSets
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    //Step - 4 - Generating Migration file --> add-migration <filename>
+    
+    //Step - 5 - Update-database
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var D = new Department() { DName = "Testing", Description = "Testing Department" };
+
+            OrganizationDbContext dbContext = new OrganizationDbContext();
+
+            //Insert oparation
+            //dbContext.Departments.Add(D);
+            //dbContext.SaveChanges();
+
+            //Read All records
+            //List<Department> departments = dbContext.Departments.ToList();
+            //foreach (var d in departments)
+            //{
+            //    Console.WriteLine($"Did:{d.Did} DName: {d.DName} Description: {d.Description}");
+            //}
+
+            //Find a record
+            Department d = dbContext.Departments.Find(2);
+            Console.WriteLine($"Did:{d.Did} DName: {d.DName} Description: {d.Description}");
+
+            //Update the record
+            d.DName = "Administrator";
+            d.Description = "New description";
+
+            dbContext.Departments.Update(d);
+            dbContext.SaveChanges();
+        }
+    }
+}
