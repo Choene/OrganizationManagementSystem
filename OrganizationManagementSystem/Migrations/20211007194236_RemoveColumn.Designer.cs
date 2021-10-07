6971c5b9acfec6273019c7c65a9bf528c8e34413
Choene <cchokoe@yahoo.com>
2021-10-07 21:44:56 +0200

    Remove column migration

--- OrganizationManagementSystem/Migrations/20211007194236_RemoveColumn.Designer.cs
+++ OrganizationManagementSystem/Migrations/20211007194236_RemoveColumn.Designer.cs
@@ -3,14 +3,16 @@ using ConsoleApp1;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace OrganizationManagementSystem.Migrations
 {
     [DbContext(typeof(OrganizationDbContext))]
-    partial class OrganizationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211007194236_RemoveColumn")]
+    partial class RemoveColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
